bd1b945949a938408279f49de57dbbddca4fc732
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-03-01 19:05:34 +0500

    set const orientation of activity_training screen

--- app/src/main/java/com/example/braintraining/Training.java
+++ app/src/main/java/com/example/braintraining/Training.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -54,6 +55,7 @@ public class Training extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         ans1 = findViewById(R.id.button1);
         ans2 = findViewById(R.id.button2);
