b085ebbb2ad59b49fa178a88e1fca0da87321957
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-03-23 21:06:23 +0500

    add tittle for about app screen

--- app/src/main/java/com/example/braintraining/AboutApp.java
+++ app/src/main/java/com/example/braintraining/AboutApp.java
@@ -1,14 +1,67 @@
 package com.example.braintraining;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class AboutApp extends AppCompatActivity {
+
+    TextView title;
+    ImageView menuIcon;
+    DrawerLayout navigationDrawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+        title = findViewById(R.id.title_of_appBar);
+
+        navigationDrawerLayout = findViewById(R.id.navigation_drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.menu_icon);
+
+        // установка заголовка экрана
+        title.setText(getResources().getString(R.string.title_of_aboutActivity));
+
+        // открытие меню по нжатию на гамбургер меню
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+                navigationDrawerLayout.openDrawer(Gravity.START);
+            }
+        });
+
+        // обработка навигации
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.about:
+                        Intent newIntent = new Intent(getApplicationContext(), AboutApp.class);
+                        startActivity(newIntent);
+                        break;
+                    case R.id.start_training:
+                        newIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(newIntent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
