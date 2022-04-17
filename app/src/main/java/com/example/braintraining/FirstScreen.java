336e5b83bbf715b5dd71b0ac9f209b9d40b0c418
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-04-17 20:12:54 +0500

    add real splashscreen

---
+++ app/src/main/java/com/example/braintraining/FirstScreen.java
@@ -0,0 +1,74 @@
+package com.example.braintraining;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class FirstScreen extends AppCompatActivity {
+
+    ImageView btnNext;
+    TextView title;
+    ImageView menuIcon;
+    DrawerLayout navigationDrawerLayout;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+
+        btnNext = findViewById(R.id.btn_next);
+        title = findViewById(R.id.title_of_appBar);
+        navigationDrawerLayout = findViewById(R.id.navigation_drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.menu_icon);
+
+        title.setText(getResources().getString(R.string.title_of_splashScreen));
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+                navigationDrawerLayout.openDrawer(Gravity.START);
+            }
+        });
+
+        // обработка навигации
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.about:
+                        Intent newIntent = new Intent(getApplicationContext(), AboutApp.class);
+                        startActivity(newIntent);
+                        break;
+                    case R.id.start_training:
+                        newIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(newIntent);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
